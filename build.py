ff42728c3442d1b84cfda6030e1bcd798b8daf0c
Alexis Rossfelder <contact.liteapplication@gmail.com>
2022-10-26 18:02:04 +0200

    Exercises format + begin shell

--- build.py
+++ build.py
@@ -3,17 +3,21 @@
 ## Imports
 import argparse
 import datetime
+from io import StringIO
 import os
 import re
 import shlex
 import shutil
 import subprocess
+import sys
 import tarfile
 import time
 from functools import reduce
 
 import colorama
-
+import pygments
+import pygments.formatters
+import pygments.lexers
 
 ## Constants
 HEADER_SEPARATOR = "_" * 20
@@ -23,6 +27,7 @@ BUILD_DIR = "build" + os.sep
 IGNORE_FOLDERS = [".git", ".vscode", "build", "venv", "__pycache__", "mp2i-pv"]
 DEFAULT_PACKAGE_NAME = "Alexis_Rossfelder_MP2I_{}.tgz"
 TP_FOLDER = "/home/alexi/Documents/MP2I/INFO/mp2i-pv/docs/TP2022-2023"
+DEFAULT_CWD = "/home/alexi/Documents/MP2I/INFO"
 UPDATE_COMMAND = ["git", "pull", "origin", "main", "--rebase"]
 EMPTY_TP = 3
 
@@ -68,6 +73,112 @@ def verbose(*args, level=1, **kwargs):
     }
     if _debug:
         levels[level](*args, **kwargs)
+
+
+def color(*args, color, end="\n", format: bool = False, **kwargs):
+    buffer = StringIO() if format else None
+    print(color, end="", file=buffer)
+    print(*args, **kwargs, end="", file=buffer)
+    print(colorama.Style.RESET_ALL, end=end, file=buffer)
+    if format and buffer is not None:
+        return buffer.getvalue()
+
+
+def print_code(code: str, language: str) -> None:
+    print(f"\n ---------  {language} code  --------- ")
+    print(
+        pygments.highlight(
+            code,
+            pygments.lexers.get_lexer_by_name(language),
+            pygments.formatters.Terminal256Formatter(),
+        ),
+    )
+    print(f" ---------  {len(language +' code') * '-'}  --------- \n")
+
+
+def latex_format(string: str) -> str:
+    """Format a latex string to be printed in the terminal"""
+    # fmt: off
+    subscript = "₀₁₂₃₄₅₆₇₈₉"
+    latex_chars = {r"\forall": "∀",r"\exists": "∃",r"\in": "∈",r"\notin": "∉",r"\subset": "⊂",r"\subseteq": "⊆",r"\supset": "⊃",r"\supseteq": "⊇",r"\emptyset": "∅",r"\cap": "∩",r"\cup": "∪",r"\infty": "∞",r"\leq": "≤",r"\geq": "≥",r"\neq": "≠",r"\equiv": "≡",r"\approx": "≈",r"\pm": "±",r"\times": "×",r"\div": "÷",r"\cdot": "·",r"\circ": "∘",r"\angle": "∠",r"\nabla": "∇",r"\partial": "∂",r"\sum": "∑",r"\prod": "∏",r"\int": "∫",r"\to": "→",r"\gets": "←",r"\leftrightarrow": "↔",r"\leftarrow": "←",r"\rightarrow": "→",r"\Rightarrow": "⇒",r"\Leftarrow": "⇐",r"\Leftrightarrow": "⇔",r"\mapsto": "↦",r"\longmapsto": "⟼",r"\varnothing": "∅",r"\alpha": "α",r"\beta": "β",r"\gamma": "γ",r"\delta": "δ",r"\epsilon": "ε",r"\zeta": "ζ",r"\eta": "η",r"\theta": "θ",r"\iota": "ι",r"\kappa": "κ",r"\lambda": "λ",r"\mu": "μ",r"\nu": "ν",r"\xi": "ξ",r"\omicron": "ο",r"\pi": "π",r"\rho": "ρ",r"\sigma": "σ",r"\tau": "τ",r"\upsilon": "υ",r"\phi": "φ",r"\chi": "χ",r"\psi": "ψ",r"\omega": "ω",r"\Alpha": "Α",r"\Beta": "Β",r"\Gamma": "Γ",r"\Delta": "Δ",r"\Epsilon": "Ε",r"\Zeta": "Ζ",r"\Eta": "Η",r"\Theta": "Θ",r"\Iota": "Ι",r"\Kappa": "Κ",r"\Lambda": "Λ",r"\Mu": "Μ",r"\Nu": "Ν",r"\Xi": "Ξ",r"\Omicron": "Ο",r"\Pi": "Π",r"\Rho": "Ρ",r"\Sigma": "Σ",r"\Tau": "Τ",r"\Upsilon": "Υ",r"\Phi": "Φ",r"\Chi": "Χ",r"\Psi": "Ψ",r"\Omega": "Ω",}
+    functions = {r"\sqrt": "√",r"\frac": "÷",r"\sin": "sin",r"\cos": "cos",r"\tan": "tan",r"\arcsin": "arcsin",r"\arccos": "arccos",r"\arctan": "arctan",r"\sinh": "sinh",r"\cosh": "cosh",r"\tanh": "tanh",r"\ln": "ln",r"\log": "log",r"\exp": "exp"}
+    superscript = "⁰¹²³⁴⁵⁶⁷⁸⁹"
+    other_superscripts = {r"\prime": "′",r"\prime\prime": "″",r"^e": "ᵉ",r"^i": "ⁱ",r"^j": "ʲ",r"^k": "ᵏ"}
+    # fmt: on
+    for latex, char in latex_chars.items():
+        string = string.replace(latex, char)
+    for latex, char in functions.items():
+        string = string.replace(latex, char)
+    for i, char in enumerate(subscript):
+        string = string.replace(f"_{i}", char)
+    for i, char in enumerate(superscript):
+        string = string.replace(f"^{i}", char)
+    for latex, char in other_superscripts.items():
+        string = string.replace(latex, char)
+
+    return string
+
+
+def print_exercises(ex_num: int, ex_title: str, ex_lines: list[str]) -> None:
+    color(
+        f"\t Exercise {ex_num} - {ex_title} ",
+        color=colorama.Back.BLACK + colorama.Style.BRIGHT,
+    )
+    print()
+    code_lines = list()
+    code_language = ""
+    for line in ex_lines:
+        if line.startswith("```"):
+            if code_language == "":
+                code_language = line[3:-1]
+            else:
+                print_code("".join(code_lines), code_language)
+                code_language = ""
+                code_lines.clear()
+            continue
+        if code_language != "":
+            code_lines.append(line)
+            continue
+
+        if "$$" in line:  # LaTeX
+            # Highlight the latex code in green
+            while "$$" in line:
+                line = line.replace("$$", colorama.Fore.GREEN, 1).replace(
+                    "$$", colorama.Fore.RESET, 1
+                )
+            line = latex_format(line)  # Format the latex code
+        if re.match(r"^\d+\.", line):  # Numbered list
+            line = color(line, end="", color=colorama.Style.BRIGHT, format=True)
+            if line is None:
+                line = ""
+
+        if re.match(r"^\[.+\]\(.+\)$", line):
+            line = color(
+                "File:",
+                line[1 : line.index("]")],
+                end="",
+                color=colorama.Fore.CYAN,
+                format=True,
+            )
+            if line is None:
+                line = ""
+        # Gray background for the code inline
+        if re.match(r"^.*\`.*\`", line):
+            line = re.sub(
+                r"`(.*?)`",
+                colorama.Back.BLACK + r"\1" + colorama.Back.RESET,
+                line,
+                16,  # Max 16 code inline per line
+            )
+
+        if "$" in line:  # LaTeX
+            # Highlight the latex code in green
+            while "$" in line:
+                line = line.replace("$", colorama.Fore.GREEN, 1).replace(
+                    "$", colorama.Fore.RESET, 1
+                )
+            line = latex_format(line)  # Format the latex code
+        print(line, end="")
 
 
 ## TP folder automations functions
@@ -119,6 +230,7 @@ def get_tp_list(path: str) -> dict[int, str]:
 def select_tp(path: str) -> tuple[str, int]:
     tp_dict = get_tp_list(path)
 
+    # TP selection
     print("Please select the file to import :")
     i = 0
     for i in sorted(tp_dict.keys()):
@@ -126,21 +238,27 @@ def select_tp(path: str) -> tuple[str, int]:
     try:
         result = input(f"Select one [{i}]: ")
         if result == "":
-            result = i
+            result = i  # Default value
         else:
             result = int(result)
         assert result in tp_dict
     except (KeyboardInterrupt, ValueError, AssertionError):
         print("\nAborting ...")
-        exit()
-    p = tp_dict[result]
+        return "", -1
+
+    # get the md file
+    p = tp_dict[result]  # tp folder by number
+
     if os.path.exists(os.path.join(p, os.path.basename(p).lower() + ".md")):
-        return os.path.join(p, os.path.basename(p).lower() + ".md"), result
+        return (
+            os.path.join(p, os.path.basename(p).lower() + ".md"),
+            result,
+        )  # Use the default file if it exists
     else:
         files = [f for f in os.listdir(p) if f.lower().endswith(".md")]
         if len(files) == 1:
-            return os.path.join(p, files[0]), result
-        elif len(files) > 1:
+            return os.path.join(p, files[0]), result  # Use the only md file
+        elif len(files) > 1:  # Ask the user to select the file
             print("Please select the file to use :")
             for i, t in enumerate(files):
                 print(f"\t{i + 1} - {os.path.basename(t)}")
@@ -149,14 +267,15 @@ def select_tp(path: str) -> tuple[str, int]:
                 assert result - 1 in range(0, len(files))
             except (KeyboardInterrupt, ValueError, AssertionError):
                 print("\nAborting ...")
-                exit()
+                return "", -1
             return os.path.join(p, files[result - 1]), result
-        else:
+        else:  # No md file found
             print("No MD file associated with this TP.")
-            return "", 0
+            return "", -1
 
 
 def read_md_tp(path: str) -> dict[str, list[str]]:
+    # Read the file and get the exercises associated with each part
     result = {"Introduction": []}
     current_part = "Introduction"
     with open(path, "r") as f:
@@ -166,6 +285,60 @@ def read_md_tp(path: str) -> dict[str, list[str]]:
                 result[current_part] = []
             elif x := re.match(r"\[.*(?:\.c|\.h)\]\((.*)\)", line):
                 result[current_part].append(x.group(1))
+    for k, v in list(result.items()):
+        if len(v) == 0:
+            del result[k]
+    return result
+
+
+def read_md_tp_ex(path: str) -> dict[str, list[tuple[int, str, list[str]]]]:
+    # Read the file and get the exercises associated with each part
+    result = {"Introduction": []}
+    current_part = "Introduction"
+    saving = False
+    current_ex = 0
+    current_title = ""
+    current_content_lines = []
+    with open(path, "r") as f:
+        line = ""
+        for line in f:
+            if x := re.match(r"\#\# (.*) -- ", line):
+                if saving:
+                    result[current_part].append(
+                        (current_ex, current_title, current_content_lines.copy())
+                    )
+                    current_content_lines.clear()
+                    print(current_part, current_title, current_ex)
+
+                current_part = x.group(1)
+                result[current_part] = []
+                saving = False
+            elif x := re.match(r"\#\#\# Exercice (\d+) : (.*)", line):
+                if saving:
+                    result[current_part].append(
+                        (current_ex, current_title, current_content_lines.copy())
+                    )
+                    current_content_lines.clear()
+                    print(current_part, current_title, current_ex)
+
+                current_ex = x.group(1)
+                current_title = x.group(2)
+                saving = True
+            elif saving:
+                current_content_lines.append(line)
+            elif current_ex != 0:
+                result[current_part].append(
+                    (current_ex, current_title, current_content_lines.copy())
+                )
+                current_content_lines.clear()
+                print(current_part, current_title, current_ex)
+
+        if saving:
+            result[current_part].append(
+                (current_ex, current_title, current_content_lines)
+            )
+            print(current_part, current_title, current_ex)
+
     for k, v in list(result.items()):
         if len(v) == 0:
             del result[k]
@@ -190,8 +363,10 @@ def prepare_folder(
 
 
 def select_files(tp_path: str) -> list[str]:
+    # read the md file
     tp_dict = read_md_tp(tp_path)
     print("Please select the categories to import :")
+    i = -2
     for i, k in enumerate(tp_dict.keys()):
         print(f"\t{i + 1} - {k}")
     try:
@@ -206,7 +381,7 @@ def select_files(tp_path: str) -> list[str]:
             result = [len(tp_dict)]
     except (KeyboardInterrupt, ValueError, AssertionError):
         print("\nAborting ...")
-        exit()
+        return []
     return reduce(
         lambda x, y: x + y, [tp_dict[list(tp_dict.keys())[i - 1]] for i in result]
     )
@@ -216,7 +391,7 @@ def select_files(tp_path: str) -> list[str]:
 def main_init(tp_folder: str, dest_folder: str) -> None:
     tp_path, tp_num = select_tp(tp_folder)
     if tp_path == "":
-        exit()
+        return
     files = select_files(tp_path)
     prepare_folder(tp_path, files, tp_num, dest_folder)
 
@@ -570,7 +745,7 @@ def package_files(files, out_path, pname):
 
 
 ## Command line interface function
-def parse_args():
+def parse_args(args: list[str] | None = None) -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="A simple C file builder and runner, with a simple syntax, and a simple way to package files",
         formatter_class=argparse.HelpFormatter,
@@ -615,15 +790,28 @@ def parse_args():
     parser.add_argument(
         "-U", "--update", action="store_true", help="Update the tp folder from git"
     )
-    return parser.parse_args()
+    parser.add_argument(
+        "-I",
+        "--interactive",
+        action="store_true",
+        help="Run in interactive mode."
+        "This opens a special shell to allow you to run commands efficiently and "
+        "overrides all other actions, and the files argument. -S is not recommended",
+    )
+    return parser.parse_args(args)
 
 
-def main():
+def main(args: list[str] | None = None, interactive: bool = False):
     global _debug, _silent
-    parsed = parse_args()
-    _debug = parsed.verbose
+    if args is None:
+        args = sys.argv[1:]
 
-    _silent = parsed.silent
+    parsed = parse_args(args)
+
+    if not interactive:
+        _debug = parsed.verbose
+
+        _silent = parsed.silent
 
     parsed.files = list(parsed.files)
     archive_pname = "TP"
@@ -641,6 +829,10 @@ def main():
     if parsed.files == ["Current directory"]:
         parsed.files = [os.getcwd()]
 
+    if parsed.interactive:
+        interactive_shell()
+        return
+
     if parsed.init:
         if not len(parsed.files) == 1 and os.path.isdir(parsed.files[0]):
             parsed.files = [os.getcwd()]
@@ -649,7 +841,7 @@ def main():
                 level=3,
             )
         main_init(TP_FOLDER, parsed.files[0])
-        exit()
+        return
 
     if len(parsed.files) == 1:
         if os.path.isdir(parsed.files[0]):
@@ -683,6 +875,20 @@ def main():
             cfile.build(not parsed.no_header)
 
 
-if __name__ == "__main__":
+def interactive_shell():
+    color(">>> Welcome to the productive mode <<<", color=colorama.Fore.BLUE)
+    color(
+        "Type 'help' or 'h' for a list of commands", color=colorama.Fore.LIGHTMAGENTA_EX
+    )
+    os.chdir(DEFAULT_CWD)
+    ex = read_md_tp_ex(
+        "/home/alexi/Documents/MP2I/INFO/mp2i-pv/docs/TP2022-2023/TP_pointeurs_et_tableaux/tp_pointeurs_et_tableaux.md"
+    )["Première étoile"]
+    for e in ex:
+        print_exercises(*e)
+    main_init(TP_FOLDER, DEFAULT_CWD)
 
-    main()
+
+if __name__ == "__main__":
+    # interactive()
+    main(sys.argv[1:])
