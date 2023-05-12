72349b6331383301bc3770b80cd2cf65ec29c244
Alexis Rossfelder <contact.liteapplication@gmail.com>
2023-05-12 13:22:43 +0200

    TP24

--- TP24/boyer_moor.c
+++ TP24/boyer_moor.c
@@ -34,13 +34,70 @@ struct liste_occ *append(struct liste_occ *liste, int occ)
     return new;
 }
 
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+bool shift_buffer(char buffer[BUFFER_SIZE], int keep_left, FILE *fichier)
+{
+    for (int i = BUFFER_SIZE - keep_left; i < BUFFER_SIZE - 1; i++)
+    {
+        printf("%c, %c -> ", buffer[i], buffer[i - BUFFER_SIZE + keep_left]);
+        buffer[i - BUFFER_SIZE + keep_left] = buffer[i];
+        printf("%c, %c\n", buffer[i], buffer[i - BUFFER_SIZE + keep_left]);
+    }
+
+    int result = fread(buffer + keep_left, sizeof(char), BUFFER_SIZE - keep_left - 1, fichier);
+    if (result == EOF)
+        buffer[result + keep_left] = '\0';
+    return result != EOF;
+}
+
 struct liste_occ *boyer_moore(FILE *fichier, const char *motif)
 {
+    assert(fichier != NULL);
+
     struct liste_occ *liste = NULL;
     char buffer[BUFFER_SIZE];
     int *occ_lettres = occ_droite(motif);
 
     int taille_motif = strlen(motif);
+    assert(taille_motif <= BUFFER_SIZE);
+
+    shift_buffer(buffer, 0, fichier);
+
+    // Algorithme de Boyer-Moore
+    int i = taille_motif - 1;
+    int j = taille_motif - 1;
+    while (true)
+    {
+        if (buffer[i] == motif[j])
+        {
+            if (j == 0)
+            {
+                liste = append(liste, i);
+                i += taille_motif;
+                j = taille_motif - 1;
+            }
+            else
+            {
+                i--;
+                j--;
+            }
+        }
+        else
+        {
+            i += taille_motif - min(j, 1 + occ_lettres[(int)buffer[i]]);
+            j = taille_motif - 1;
+            if (i >= BUFFER_SIZE)
+            {
+                if (!shift_buffer(buffer, taille_motif - 1, fichier))
+                    break;
+                i = taille_motif - 1;
+            }
+        }
+    }
 
     return liste;
 }
--- TP24/boyer_moor.h
+++ TP24/boyer_moor.h
@@ -3,8 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
 
-#define BUFFER_SIZE 64
+#define BUFFER_SIZE 16
 
 struct liste_occ
 {
@@ -14,10 +17,13 @@ struct liste_occ
 
 struct liste_occ *append(struct liste_occ *liste, int occ);
 
+int min(int, int);
+bool shift_buffer(char buffer[BUFFER_SIZE], int keep_left, FILE *fichier);
+
 int *occ_droite(const char *motif);
 
 struct liste_occ *boyer_moore(
-    FILE *fichier,     // fichier à disséquer
-    const char *motif, // motif à chercher
+    FILE *fichier,    // fichier à disséquer
+    const char *motif // motif à chercher
 );
 #endif // __BOYER_MOOR_H
